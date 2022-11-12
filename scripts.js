d57dfb4e7e751d1fe93a94c18a7d2c67462ebce6
Huy Cao <caogiahuy615@gmail.com>
2022-11-12 15:17:24 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-const cactus2 = document.getElementById("cactus2");
-const cactus3 = document.getElementById("cactus3");
+const obs = document.getElementById("obs");
+const obs2 = document.getElementById("obs2");
+const obs3 = document.getElementById("obs3");
 
 function jump() {
   if (dino.classList != "jump") {
@@ -15,32 +15,32 @@ function jump() {
 
 function create1() {
 
-  if (cactus.classList != "create") {
-    cactus.classList.add("create");
+  if (obs.classList != "create") {
+    obs.classList.add("create");
 
     setTimeout(function () {
-      cactus.classList.remove("create");
-    }, 1000);
+      obs.classList.remove("create");
+    }, 10000);
   }
 }
 
 function create2() {
-  if (cactus2.classList != "create2") {
-    cactus2.classList.add("create2");
+  if (obs2.classList != "create2") {
+    obs2.classList.add("create2");
 
     setTimeout(function () {
-      cactus2.classList.remove("create2");
+      obs2.classList.remove("create2");
     }, 5000);
   }
 }
 
 function create3() {
-  if (cactus3.classList != "create3") {
-    cactus3.classList.add("create3");
+  if (obs3.classList != "create3") {
+    obs3.classList.add("create3");
 
     setTimeout(function () {
-      cactus3.classList.remove("create3");
-    }, 5000);
+      obs3.classList.remove("create3");
+    }, 2000);
   }
 }
 // let isAlive = setInterval(function () {
@@ -64,13 +64,13 @@ function create3() {
 document.addEventListener("keypress", (event)=>{
   var name = event.key
   var code = event.code
-  if (name === "r") {
+  if (name === "e") {
     create1();
   }
-  if (name === "t") {
+  if (name === "r") {
     create2();
   }
-  if (name === "y"){
+  if (name === "t"){
     create3();
   }
 });
